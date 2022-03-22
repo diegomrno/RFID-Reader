224ba2f4b683b6a774b97cd2a51366666695624a
DiegoMoreno0640 <diegomoreno.pro45@gmail.com>
2022-03-22 10:24:23 +0000

    update: add buzzer callback

--- Reader/ReaderScript.cpp
+++ Reader/ReaderScript.cpp
@@ -1,5 +1,6 @@
 /*
-    /!\ To Arduino /!\
+    Buzzer on Socket D6
+    RFID Reader on Socket D2
 */
  
 #include <SoftwareSerial.h>
@@ -11,7 +12,8 @@ int count = 0;
 void setup()
 {
     SoftSerial.begin(9600);   
-    Serial.begin(9600);         
+    Serial.begin(9600);    
+    pinMode(6, OUTPUT);
 }
  
 void loop()
@@ -22,13 +24,17 @@ void loop()
         {
             buffer[count++] = SoftSerial.read();      
             if(count == 64)break;
+            
+            digitalWrite(6, HIGH);
+            delay(1000);
+            digitalWrite(6, LOW);
         }
         Serial.write(buffer, count);    
         clearBufferArray();             
         count = 0;                      
     }
     if (Serial.available())             
-    SoftSerial.write(Serial.read());    
+    SoftSerial.write(Serial.read()); 
 }
 void clearBufferArray()                 
 {
